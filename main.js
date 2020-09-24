3fd453af763d5f24473e8469ff01a782f62fd22a
Madisyn-0 <madimuggeridge@gmail.com>
2020-09-24 14:59:39 +1200

    Tried to add js

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
+function changeMode() {
+  const screen = document.querySelectorAll(".screen");
+  const july = document.querySelectorAll(".julyM");
+  const month = document.querySelectorAll(".month");
+  const blogEntry = document.querySelectorAll(".blog-entry");
+  const blogEntry2 = document.querySelectorAll(".blog-entry2");
+  
+if(document.getElementById("july").getAttribute("class") === "julyM"){
+  makeDark(screen);
+  makeDark(july);
+  makeDark(month);
+  makeDark(blogEntry);
+  makeDark(blogEntry2);
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+  makeLight(screen);
+  makeLight(july);
+  makeLight(month);
+  makeLight(blogEntry);
+  makeLight(blogEntry2);
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function makeDark(element){
+  element.forEach(function(el) {
+      el.classList.remove("julyM");
+      el.classList.add("all");
+  });
+}
+
+function makeLight(element){
+  element.forEach(function(el) {
+      el.classList.remove("all");
+      el.classList.add("julyM");
+  });
 }
