0260e4627eed6ebba05e20c0570244c999b1c435
Madisyn-0 <madimuggeridge@gmail.com>
2020-10-04 14:43:53 +1300

    Attempted mobile nav

--- main.js
+++ main.js
@@ -62,3 +62,15 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
